dc6fe38f8a78c71c88cfd6226baba570cba311a7
moonboymoney <89177222+moonboymoney@users.noreply.github.com>
2021-08-18 23:20:18 -0500

    Initialize gh-pages

---
+++ precache-manifest.d7ecb14168d1b90694cc1d60d44c040a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ba916587f33cc3bf9b2a39b5c1484a",
+    "url": "/pow-faucet/index.html"
+  },
+  {
+    "revision": "35722b6a388954c3a10d",
+    "url": "/pow-faucet/static/js/2.4575f308.chunk.js"
+  },
+  {
+    "revision": "7e425facb6be6c447caa367bb7d042df",
+    "url": "/pow-faucet/static/js/2.4575f308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2280da3292b9fd183038",
+    "url": "/pow-faucet/static/js/main.1dcb637c.chunk.js"
+  },
+  {
+    "revision": "79e09f6c2809724e89d3",
+    "url": "/pow-faucet/static/js/runtime-main.9089d174.js"
+  }
+]);
